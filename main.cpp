07a0d709b4187ca4dc86c51acde4f5d831f294c9
Georgi Georgiev <georgi8georgiev@gmail.com>
2018-11-05 23:51:04 +0200

    Reorganizations.

--- main.cpp
+++ main.cpp
@@ -7,19 +7,24 @@
 
 int init_udev(udev** udev)
 {
-    *udev = udev_new();
+    int result = -1;
 
-    return  (udev != nullptr);
+    *udev = udev_new();
+    if (*udev != nullptr)
+    {
+        result = 0;
+    }
+
+    return result;
 }
 
-//int scan()
-//{
-
-//}
+int setup_udev_monitor(const char* monitored_subsystem, udev* udev, udev_monitor** udev_monitor, int* udev_monitor_fd)
+{
+    return 0;
+}
 
 int main (void)
 {
-    struct udev_enumerate *udev_enumeration = nullptr;
     struct udev_device *dev = nullptr;
     struct udev_monitor *udev_monitor = nullptr;
     int udev_monitor_fd = 0;
@@ -27,7 +32,7 @@ int main (void)
 
     // Init library
     struct udev *udev = nullptr;
-    if (!init_udev(&udev))
+    if (0 != init_udev(&udev))
     {
         return 0;
     }
@@ -41,24 +46,15 @@ int main (void)
 
 
     // Create a list of the devices in the tty subsystem
-    udev_enumeration = udev_enumerate_new(udev);
+    struct udev_enumerate* udev_enumeration = udev_enumerate_new(udev);
     // Set filter
     udev_enumerate_add_match_subsystem(udev_enumeration, "tty");
     // Enumerate
     udev_enumerate_scan_devices(udev_enumeration);
 
-
-
-    struct udev_list_entry *device_list_entries = nullptr;
+    //
     struct udev_list_entry *device_list_entry = nullptr;
-
-    device_list_entries = udev_enumerate_get_list_entry(udev_enumeration);
-    /* For each item enumerated, print out its information.
-       udev_list_entry_foreach is a macro which expands to
-       a loop. The loop will be executed for each member in
-       devices, setting dev_list_entry to a list entry
-       which contains the device's path in /sys. */
-
+    struct udev_list_entry *device_list_entries = udev_enumerate_get_list_entry(udev_enumeration);
 
     udev_list_entry_foreach(device_list_entry, device_list_entries)
     {
@@ -69,51 +65,30 @@ int main (void)
         path = udev_list_entry_get_name(device_list_entry);
         dev = udev_device_new_from_syspath(udev, path);
 
-        // usb_device_get_devnode() returns the path to the device node itself in /dev. */
-
-
-        /* The device pointed to by dev contains information about
-           the hidraw device. In order to get information about the
-           USB device, get the parent device with the
-           subsystem/devtype pair of "usb"/"usb_device". This will
-           be several levels up the tree, but the function will find
-           it.*/
+        //  The device pointed to by dev contains information about the tty device. In order to get information about the
+        // USB device, get the parent device with the subsystem/devtype pair of "usb"/"usb_device".
+        // This will be several levels up the tree, but the function will find it.
         usb_dev = udev_device_get_parent_with_subsystem_devtype(dev, "usb", "usb_device");
-        if (!usb_dev)
+        if (usb_dev)
         {
-            //printf("Unable to find parent usb device.");
-            //exit(1);
-        }
-        else
-        {
+            printf("   Node: %s\n", udev_device_get_devnode(dev));
+            printf("   idVendor: %s\n",     udev_device_get_sysattr_value(usb_dev,"idVendor"));
+            printf("   idProduct: %s\n",    udev_device_get_sysattr_value(usb_dev, "idProduct"));
+            printf("   Manufacturer: %s\n", udev_device_get_sysattr_value(usb_dev,"manufacturer"));
+            printf("   Product: %s\n",      udev_device_get_sysattr_value(usb_dev,"product"));
+            printf("   Serial: %s\n",       udev_device_get_sysattr_value(usb_dev, "serial"));
 
-            printf("Device Node Path: %s\n", udev_device_get_devnode(dev));
-
-            /* From here, we can call get_sysattr_value() for each file
-           in the device's /sys entry. The strings passed into these
-           functions (idProduct, idVendor, serial, etc.) correspond
-           directly to the files in the /sys directory which
-           represents the USB device. Note that USB strings are
-           Unicode, UCS2 encoded, but the strings returned from
-           udev_device_get_sysattr_value() are UTF-8 encoded. */
-            printf("  VID/PID: %s %s\n",
-                   udev_device_get_sysattr_value(usb_dev,"idVendor"),
-                   udev_device_get_sysattr_value(usb_dev, "idProduct"));
-            printf("  %s\n  %s\n",
-                   udev_device_get_sysattr_value(usb_dev,"manufacturer"),
-                   udev_device_get_sysattr_value(usb_dev,"product"));
-            printf("  serial: %s\n",
-                   udev_device_get_sysattr_value(usb_dev, "serial"));
-  printf("   Action: %s\n", udev_device_get_action(dev));
             /* unref only the original dev, not the parent device;
            the latter will be unreferenced automatically if set. */
             udev_device_unref(dev);
         }
     }
-    /* Free the enumerator object */
+    // Free the enumerator object
     udev_enumerate_unref(udev_enumeration);
 
-    /* Begin polling for udev events. Events occur when deevices
+
+
+    /* Begin polling for udev events. Events occur when devices
        attached to the system are added, removed, or change state.
        udev_monitor_receive_device() will return a device
        object representing the device which changed and what type of
@@ -152,12 +127,7 @@ int main (void)
             {
                 struct udev_device *usb_dev;
                 usb_dev = udev_device_get_parent_with_subsystem_devtype(dev, "usb", "usb_device");
-                if (!usb_dev)
-                {
-                    //printf("Unable to find parent usb device.");
-                    //exit(1);
-                }
-                else
+                if (usb_dev)
                 {
                     printf("\n\nDevice\n");
                     printf("   Node: %s\n", udev_device_get_devnode(dev));
@@ -178,10 +148,6 @@ int main (void)
                 // unref only the original dev, not the parent device, the latter will be unreferenced automatically if set. */
                 udev_device_unref(dev);
             }
-            else
-            {
-                printf("No Device from receive_device(). An error occured.\n");
-            }
         }
 
         usleep(250*1000);
@@ -193,3 +159,4 @@ int main (void)
 
     return 0;
 }
+
